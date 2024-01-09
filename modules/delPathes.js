fcf752fbe4ea1f00c5bc228082821dd81e86de08
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-01-09 13:05:38 +0300

    fix delPathes.js

--- modules/delPathes.js
+++ modules/delPathes.js
@@ -4,13 +4,13 @@ async function delPathes(unpackedWim, config) {
   for (const element of config.pathsToDelete) {
     if (element.IsFolder) {
       const delPathScript = `takeown /f "${unpackedWim}${element.Path}" /r /d Y | Out-Null
-      icacls "${unpackedWim}${element.Path}" /grant ("$env:username"+":F") /T /C | Out-Null
-      Remove-Item -Force "${unpackedWim}${element.Path}" -Recurse -ErrorAction SilentlyContinue | Out-Null`;
+icacls "${unpackedWim}${element.Path}" /grant ("$env:username"+":F") /T /C | Out-Null
+Remove-Item -Force "${unpackedWim}${element.Path}" -Recurse -ErrorAction SilentlyContinue | Out-Null`;
       await runPowerShellScript(delPathScript);
     } else {
       const delPathScript = `takeown /f "${unpackedWim}${element.Path}" | Out-Null
-      icacls "${unpackedWim}${element.Path}" /grant ("$env:username"+":F") /T /C | Out-Null
-      Remove-Item -Force "${unpackedWim}${element.Path}" -ErrorAction SilentlyContinue | Out-Null`;
+icacls "${unpackedWim}${element.Path}" /grant ("$env:username"+":F") /T /C | Out-Null
+Remove-Item -Force "${unpackedWim}${element.Path}" -ErrorAction SilentlyContinue | Out-Null`;
       await runPowerShellScript(delPathScript);
     }
   }
